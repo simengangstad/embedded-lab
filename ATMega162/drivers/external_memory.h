472ee7b6768fc973f1eb5acb4737ae879ab10414
Geir Ola Lillestøl Tvinnereim <gotvinne@ntnu.no>
2020-09-25 17:11:47 +0200

    Move sram test back to external memory

--- ATMega162/drivers/external_memory.h
+++ ATMega162/drivers/external_memory.h
@@ -14,16 +14,16 @@
 void external_memory_init();
 
 /**
- * @brief Writes a byte of data to external memory. 
- * 
- * @param data The byte to write. 
- * @param offset_address The address to write to, offset by base address 0x1000. 
+ * @brief Writes a byte of data to external memory.
+ *
+ * @param data The byte to write.
+ * @param offset_address The address to write to, offset by base address 0x1000.
  */
 void external_memory_write(uint8_t data, uint16_t offset_address);
 
 /**
  * @brief Retrieve a value from memory.
- * 
+ *
  * @param offset_address The data address, offset by base address 0x1000.
  *
  * @return The data at the given address.
@@ -33,6 +33,6 @@ uint8_t external_memory_read(uint16_t offset_address);
 /**
  * @brief Tests the memory space by writing and reading to different locations.
  */
-void external_memory_test();
+void external_memory_sram_test();
 
 #endif
