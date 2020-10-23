2b1ae4cb2a4eeabec57530280b6a9ae30a3ba869
Geir Ola Tvinnereim <gotvinne@ntnu.no>
2020-10-23 20:55:19 +0200

    Updated todo

--- ATSAM3X8E/Main/main.c
+++ ATSAM3X8E/Main/main.c
@@ -17,8 +17,8 @@ void ATSAM_leds(void) {
     PMC->PMC_PCER0 = PMC_PCER0_PID11;
 
     // Enable pins
-    PIOA->PIO_PER = PIO_PA19;
-    PIOA->PIO_PER = PIO_PA20;
+    PIOA->PIO_PER = PIO_PA19; //PIO:PER_P19
+    PIOA->PIO_PER = PIO_PA20; //PIO_PER_P20
 
     // Set to output
     PIOA->PIO_OER = PIO_PA19;
@@ -34,6 +34,8 @@ void ATSAM_INIT(void) {
     configure_uart();
     pwm_init();
     adc_init();
+    motor_init();
+    motor_dac_init(); 
     // ATSAM_leds();
 
     // Disable watchdog
@@ -43,18 +45,20 @@ void ATSAM_INIT(void) {
 int main(void) {
     ATSAM_INIT();
     can_init_def_tx_rx_mb(ATSAM_CAN_BR);
+    motor_set_actuation(0x0);
 
     while (1) {
         CAN_MESSAGE can_message;
         if (can_receive(&can_message, 0)) {
             if (can_message.id == 1) {
-                // printf("X: %d, Y: %d\n\r", (int8_t)can_message.data[0], (int8_t)can_message.data[1]);
-                // printf("Direction %i\n\r", can_message.data[2]);
+                //printf("X: %d, Y: %d\n\r", (int8_t)can_message.data[0], (int8_t)can_message.data[1]);
+                //printf("Direction %i\n\r", can_message.data[2]);
                 servo_send_duty_cycle((int8_t)can_message.data[0]);
             }
         }
 
         game_update();
+
     }
 
     return 0;
