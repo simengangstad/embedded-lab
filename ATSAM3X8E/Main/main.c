9d2acba2b77fcc0aa52430d891ff51b45214a6e6
Geir Ola Tvinnereim <gotvinne@ntnu.no>
2020-10-23 20:56:37 +0200

    Made joystick test code

--- ATSAM3X8E/Main/main.c
+++ ATSAM3X8E/Main/main.c
@@ -4,6 +4,8 @@
 #include "drivers/actuator/servo.h"
 #include "drivers/adc.h"
 #include "drivers/can/can_controller.h"
+#include "drivers/can/can_joystick.h"
+#include "drivers/actuator/motor_controller.h"
 #include "drivers/uart_and_printf/printf-stdarg.h"
 #include "drivers/uart_and_printf/uart.h"
 #include "game.h"
@@ -17,8 +19,8 @@ void ATSAM_leds(void) {
     PMC->PMC_PCER0 = PMC_PCER0_PID11;
 
     // Enable pins
-    PIOA->PIO_PER = PIO_PA19; //PIO:PER_P19
-    PIOA->PIO_PER = PIO_PA20; //PIO_PER_P20
+    PIOA->PIO_PER = PIO_PA19;  // PIO:PER_P19
+    PIOA->PIO_PER = PIO_PA20;  // PIO_PER_P20
 
     // Set to output
     PIOA->PIO_OER = PIO_PA19;
@@ -35,8 +37,7 @@ void ATSAM_INIT(void) {
     pwm_init();
     adc_init();
     motor_init();
-    motor_dac_init(); 
-    // ATSAM_leds();
+    ATSAM_leds();
 
     // Disable watchdog
     WDT->WDT_MR = WDT_MR_WDDIS;
@@ -45,20 +46,29 @@ void ATSAM_INIT(void) {
 int main(void) {
     ATSAM_INIT();
     can_init_def_tx_rx_mb(ATSAM_CAN_BR);
-    motor_set_actuation(0x0);
+    // motor_set_actuation(0xFFFF);
 
     while (1) {
-        CAN_MESSAGE can_message;
-        if (can_receive(&can_message, 0)) {
-            if (can_message.id == 1) {
-                //printf("X: %d, Y: %d\n\r", (int8_t)can_message.data[0], (int8_t)can_message.data[1]);
-                //printf("Direction %i\n\r", can_message.data[2]);
-                servo_send_duty_cycle((int8_t)can_message.data[0]);
-            }
-        }
+        // Dette var egentlig feil, man maa ha not (1) i den oeverste if-en. Men hvorfor funket det?
 
-        game_update();
+        // CAN_MESSAGE can_message;
+        // if (can_receive(&can_message, 0)) {
+        //     if (can_message.id == 1) {
+        //         printf("x: %d, y: %d, dir: %d\n\r", (int8_t)can_message.data[0], (int8_t)can_message.data[1],
+        //                (JoystickDirection)can_message.data[2]);
+        //         servo_send_duty_cycle((int8_t)can_message.data[0]);
+        //     }
+        // }
 
+        Joystick joystick;
+        can_joystick_read(&joystick);
+        servo_send_duty_cycle(joystick.y);
+
+        motor_open_loop_speed_control(&joystick);
+		
+		printf("joystick x: %d\r\n", joystick.x); 
+
+        //game_update();
     }
 
     return 0;
