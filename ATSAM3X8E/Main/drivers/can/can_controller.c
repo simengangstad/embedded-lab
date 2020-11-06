c784c14b1db3bc5b8f9bcc1ab74a5605598beb60
Geir Ola Tvinnereim <gotvinne@ntnu.no>
2020-11-06 11:04:44 +0100

    Fix newline

--- ATSAM3X8E/Main/drivers/can/can_controller.c
+++ ATSAM3X8E/Main/drivers/can/can_controller.c
@@ -180,8 +180,7 @@ uint8_t can_receive(CAN_MESSAGE* can_msg, uint8_t rx_mb_id) {
         CAN0->CAN_MB[rx_mb_id].CAN_MMR = CAN_MMR_MOT_MB_RX;
         CAN0->CAN_MB[rx_mb_id].CAN_MCR |= CAN_MCR_MTCR;
         return 0;
-    } else  // Mailbox busy
-    {
+    } else {  // Mailbox busy
         return 1;
     }
 }
